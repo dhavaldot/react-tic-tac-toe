ca3cd21c480961589b89213fdeb37f307db8fe9a
dhavaldot <dhaval.dotinfotech@gmail.com>
2021-07-23 19:24:54 +0530

    Time travel to step

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -8,29 +8,43 @@ const styles = {
 };
 
 const Game = () => {
-	const [board, setBoard] = useState(Array(9).fill(null));
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const [stepNumber, setStepNumber] = useState(0);
 	const [xIsNext, setXisNext] = useState(true);
-	const winner = calculateWinner(board);
+	const winner = calculateWinner(history[stepNumber]);
 
 	const handleClick = (i) => {
-		const boardCopy = [...board];
+		const timeInHistory = history.slice(0, stepNumber + 1);
+		const current = timeInHistory[stepNumber];
 
-		if (winner || boardCopy[i]) return;
+		const squares = [...current];
 
-		boardCopy[i] = xIsNext ? 'X' : 'O';
-		setBoard(boardCopy);
+		if (winner || squares[i]) return;
+
+		squares[i] = xIsNext ? 'X' : 'O';
+		setHistory([...timeInHistory, squares]);
+		setStepNumber(timeInHistory.length);
 		setXisNext(!xIsNext);
 	};
 
-	const jumpTo = () => {};
+	const jumpTo = (step) => {
+		setStepNumber(step);
+		setXisNext(step % 2 === 0);
+	};
 
-	const renderMoves = () => (
-		<button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>
-	);
+	const renderMoves = () =>
+		history.map((_step, move) => {
+			const destination = move ? `Go to move#${move}` : `Go to start`;
+			return (
+				<li key={move}>
+					<button onClick={() => jumpTo(move)}>{destination}</button>
+				</li>
+			);
+		});
 
 	return (
 		<>
-			<Board squares={board} onClick={handleClick} />
+			<Board squares={history[stepNumber]} onClick={handleClick} />
 			<div style={styles}>
 				<p>
 					{winner
